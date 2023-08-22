05c43d0a459c1311b14df32f0421327eb7dd035f
Ywenn <cshn2215393@qq.com>
2023-08-22 15:15:51 +0800

    my Third commit

---
+++ src/main/java/com/powernode/animal/service/impl/AnimalServletImpl.java
@@ -0,0 +1,16 @@
+package com.powernode.animal.service.impl;
+
+import com.powernode.animal.service.AnimalServlet;
+
+/**
+ * @version 1.0
+ * @Author Ywenn
+ * @Date 2023/8/22 15:14
+ * @注释
+ */
+public class AnimalServletImpl implements AnimalServlet {
+    @Override
+    public void addAnimalMasterName() {
+        System.out.println("这是一个添加动物主人的业务代码!");
+    }
+}
