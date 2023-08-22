727bbd98fbab9507cf48b2f1911b8f216f1f4513
Ywenn <cshn2215393@qq.com>
2023-08-22 15:08:09 +0800

    my Second commit

---
+++ src/main/java/com/powernode/animal/service/AnimalServlet.java
@@ -0,0 +1,12 @@
+package com.powernode.animal.service;
+
+/**
+ * @version 1.0
+ * @Author Ywenn
+ * @Date 2023/8/22 15:06
+ * @注释
+ */
+public interface AnimalServlet {
+
+    void addAnimalMasterName();
+}
