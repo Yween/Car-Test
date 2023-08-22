2d4a1297dca0047d9899745766c4f6d53c125f74
Ywenn <cshn2215393@qq.com>
2023-08-22 15:03:54 +0800

    my First commit

---
+++ src/main/java/com/powernode/animal/pojo/Car.java
@@ -0,0 +1,67 @@
+package com.powernode.animal.pojo;
+
+/**
+ * @version 1.0
+ * @Author Ywenn
+ * @Date 2023/8/22 14:53
+ * @注释
+ */
+public class Car {
+
+    private String name;
+    private String kind;
+    private int age;
+    private String master;
+
+    public Car() {
+    }
+
+    public Car(String name, String kind, int age, String master) {
+        this.name = name;
+        this.kind = kind;
+        this.age = age;
+        this.master = master;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getMaster() {
+        return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", kind='" + kind + '\'' +
+                ", age=" + age +
+                ", master='" + master + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/powernode/animal/test/AnimalTest.java
@@ -0,0 +1,13 @@
+package com.powernode.animal.test;
+
+/**
+ * @version 1.0
+ * @Author Ywenn
+ * @Date 2023/8/22 14:56
+ * @注释
+ */
+public class AnimalTest {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试动物的代码！");
+    }
+}
